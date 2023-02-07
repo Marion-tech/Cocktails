496899bc79b59c4ca44dfdac6399d9a990bf285a
Marion-tech <cainael@gmail.com>
2023-02-07 18:50:51 +0100

    Dyma-app-cocktail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { ICocktail } from './interfaces/cocktail.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Cocktails';
-}
+export class AppComponent {}
---
+++ src/app/interfaces/cocktail.interface.ts
@@ -0,0 +1,5 @@
+export interface ICocktail {
+  name: string;
+  img: string;
+  description: string;
+}
